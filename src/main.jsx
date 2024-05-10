fb8244fd35c9322dc62417a31effa6ac75fae505
VictorGabriel2022 <mazmorra13rol@gmail.com>
2024-05-10 16:28:33 -0500

    first commit3

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ const productList = [
   { id: 5, name: 'Producto 2', price: 20 ,img:"https://www.visioncenter.com.pe/cdn/shop/products/8056597815857_1_257c2980-b1f3-46ec-b0f8-e03830747f96_720x720.png?v=1709130275"},
   { id: 6, name: 'Producto 3', price: 30 ,img:"https://www.visioncenter.com.pe/cdn/shop/files/4044709509699_1_720x720.jpg?v=1713438006"},
   { id: 7, name: 'Producto 4', price: 50 ,img:"https://www.visioncenter.com.pe/cdn/shop/files/4044709510626_1_720x720.png?v=1713435470" },
-  { id: 6, name: 'Producto 3', price: 30 ,img:"https://www.visioncenter.com.pe/cdn/shop/files/4044709509699_1_720x720.jpg?v=1713438006"},
+  { id: 8, name: 'Producto 3', price: 30 ,img:"https://www.visioncenter.com.pe/cdn/shop/files/4044709509699_1_720x720.jpg?v=1713438006"},
 ];
 ReactDOM.createRoot(document.getElementById('root')).render(
   
